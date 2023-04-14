76f3f26627deff62ada3ca3f79dbced32fc4a3b5
Zohaib <zrana1214@gmail.com>
2023-04-14 10:15:13 -0400

    moving file around for git pages

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ let ampHistory = []
 =============================================*/
 
 function preload() {
-	audio = loadSound("/sun_ra_ha_female.wav");
+	audio = loadSound("/GUZARISH.wav");
 }
 
 // function uploaded(file) {
